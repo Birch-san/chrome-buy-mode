cbc277194d8bb3c7aa25d2adf845fa0e71ab5e95
Alex Birch <vengeance.mx@gmail.com>
2016-04-04 01:13:21 +0100

    communication

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -1,7 +1,3 @@
-console.log("SUP YO");
-console.log("SUP YO");
-console.log("SUP YO");
-console.log("SUP YO");
 chrome.extension.sendMessage({}, function(response) {
 	var readyStateCheckInterval = setInterval(function() {
 	if (document.readyState === "complete") {
@@ -10,8 +6,9 @@ chrome.extension.sendMessage({}, function(response) {
 		// ----------------------------------------------------------
 		// This part of the script triggers when page is done loading
 		console.log("Hello. This message was sent from scripts/inject.js");
+		console.log(response);
 		// ----------------------------------------------------------
-
+		
 	}
 	}, 10);
 });
