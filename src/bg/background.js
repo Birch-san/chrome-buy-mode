e78544d4e5da188f5ce610d00c9fb154234130e3
Alex Birch <vengeance.mx@gmail.com>
2016-04-05 23:48:22 +0100

    king of events

--- src/bg/background.js
+++ src/bg/background.js
@@ -6,20 +6,71 @@
 
 
 var audio = new Audio('../../music/1.mp3');
-var playing = false;
+audio.loop = true;
+
+function ensurePlaying() {
+	if (audio.paused) {
+		audio.play();
+	}
+}
+
+function ensureNotPlaying() {
+	if (!audio.paused) {
+		audio.pause();
+	}
+}
+
+// function stop() {
+// 	if (!playing) {
+// 		audio.play();
+// 		playing = true;
+// 	}
+// }
+
 //example of using a message handler from the inject scripts
 chrome.webNavigation.onCompleted.addListener(
   function(details) {
-  	// chrome.pageAction.show(sender.tab.id);
-  	if (!playing) {
-		audio.play();
-		playing = true;
+  	ensureMusicPlayingIffRequired();
+  });
+
+function ensureMusicPlayingIffRequired() {
+	function callback(tabs) {
+		if (tabs.length > 0) {
+			ensurePlaying();
+		} else {
+			ensureNotPlaying();
+		}
 	}
-  }, {
-    url: [{
-        hostContains: '.amazon.'
-    },
-    {
-        hostContains: 'birchlabs.'
-    }]
+	var queryInfo = {
+		active: true,
+		currentWindow: true,
+		// status: 'complete',
+		url: [
+		"*://*.amazon.co.uk/*",
+	    "*://*.amazon.com/*",
+	    "*://*.birchlabs.co.uk/*",
+	    "*://birchlabs.co.uk/*"
+		]
+	};
+	chrome.tabs.query(queryInfo, callback);
+}
+
+chrome.tabs.onRemoved.addListener(function(tabId, attachInfo) {
+	ensureMusicPlayingIffRequired();
+});
+
+chrome.windows.onFocusChanged.addListener(function(tabId, attachInfo) {
+	ensureMusicPlayingIffRequired();
+});
+
+// chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
+// 	ensureMusicPlayingIffRequired();
+// });
+
+chrome.tabs.onActivated.addListener(function(activeInfo) {
+	//activeInfo
+	// int tabId The ID of the tab that has become active
+	// int windowId The ID of the window the active tab changed inside of.
+
+	ensureMusicPlayingIffRequired();
 });
