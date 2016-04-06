ad4386528e7a362d83814fe4a8472456e4dec9e9
Alex Birch <vengeance.mx@gmail.com>
2016-04-06 20:11:52 +0100

    progress

--- src/options/options.js
+++ src/options/options.js
@@ -2,7 +2,7 @@ angular
 .module('optionsApp', [])
 .controller('HtmlCtrl', ['$scope', function($scope) {
 	angular.extend($scope, {
-		lang: chrome.i18n.getUILanguage(),
+		lang: chrome.i18n.getMessage("@@ui_locale"),
 		localized: function(key) {
 			return chrome.i18n.getMessage(key);
 		}
