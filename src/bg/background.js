8c587831d3b254583256d502a237718187ad8db4
Alex Birch <vengeance.mx@gmail.com>
2016-04-07 01:11:09 +0100

    responds to runtime changes in settings, what.

--- src/bg/background.js
+++ src/bg/background.js
@@ -4,6 +4,8 @@
 //     "sample_setting": "This is how you use Store.js to remember values"
 // });
 
+var alive = false;
+
 var state;
 
 function pickRandomSong(rule) {
@@ -44,16 +46,17 @@ function ensureNotPlaying(audio) {
 	}
 }
 
-//example of using a message handler from the inject scripts
-chrome.webNavigation.onCompleted.addListener(
-  function(details) {
-  	ensureMusicPlayingIffRequired();
-  });
-
 function ensureMusicPlayingIffRequired() {
+	if (!alive) {
+		return;
+	}
+
 	for (var i=0; i<state.rules.length; i++) {
 		var rule = state.rules[i];
 		function callback(tabs) {
+			if (!alive) {
+				return;
+			}
 			if (tabs.length > 0) {
 				ensurePlaying(rule.audio);
 			} else {
@@ -72,73 +75,96 @@ function ensureMusicPlayingIffRequired() {
 	}
 }
 
-chrome.storage.sync.get("state", function(items) {
-	try {
-		state = JSON.parse(items.state);
-	} catch(err) {
-	}
-	if (!state) {
-		state = {};
-	}
-	if (!state.rules || !state.rules.length) {
-		state.rules = 
-		[
-		{
-			"match": [
-		    "*://*.birchlabs.co.uk/*",
-		    "*://birchlabs.co.uk/*"
-			],
-			"playlist":
-			[
-			'../../music/elevator.mp3'
-			]
+function start() {
+	chrome.storage.sync.get("state", function(items) {
+		try {
+			state = JSON.parse(items.state);
+		} catch(err) {
 		}
-		];
-		// {
-		// 	"match": [
-		// 	"*://*.amazon.co.uk/*",
-		//     "*://*.amazon.com/*",
-		//     "*://*.birchlabs.co.uk/*",
-		//     "*://birchlabs.co.uk/*"
-		// 	],
-		// 	"playlist":
-		// 	[
-		// 	'file:///Users/birch/git/chrome-buy-mode/music/1.mp3',
-		// 	'file:///Users/birch/git/chrome-buy-mode/music/2.mp3',
-		// 	'file:///Users/birch/git/chrome-buy-mode/music/3.mp3',
-		// 	'file:///Users/birch/git/chrome-buy-mode/music/4.mp3'
-		// 	],
-		// 	"audio": undefined,
-		// 	"favouriteFirst": true
-		// }
+		if (!state) {
+			state = {};
+		}
+		if (!state.rules || !state.rules.length) {
+			state.rules = 
+			[
+			{
+				"match": [
+			    "*://*.birchlabs.co.uk/*",
+			    "*://birchlabs.co.uk/*"
+				],
+				"playlist":
+				[
+				'../../music/elevator.mp3'
+				]
+			}
+			];
+			// {
+			// 	"match": [
+			// 	"*://*.amazon.co.uk/*",
+			//     "*://*.amazon.com/*",
+			//     "*://*.birchlabs.co.uk/*",
+			//     "*://birchlabs.co.uk/*"
+			// 	],
+			// 	"playlist":
+			// 	[
+			// 	'file:///Users/birch/git/chrome-buy-mode/music/1.mp3',
+			// 	'file:///Users/birch/git/chrome-buy-mode/music/2.mp3',
+			// 	'file:///Users/birch/git/chrome-buy-mode/music/3.mp3',
+			// 	'file:///Users/birch/git/chrome-buy-mode/music/4.mp3'
+			// 	],
+			// 	"audio": undefined,
+			// 	"favouriteFirst": true
+			// }
+		}
+
+		(function initRules(rules) {
+			for (var i=0; i<rules.length; i++) {
+				var rule = rules[i];
+				rule.audio = new Audio();
+				var audio = rule.audio;
+				cue(audio, (rule.favouriteFirst && rule.playlist[0]) || pickNextSong(rule));
+
+				audio.addEventListener('ended', function() {	
+					cue(pickNextSong());
+				});
+			}
+		})(state.rules);
+
+		alive = true;
+	})
+}
+
+chrome.tabs.onRemoved.addListener(function(tabId, attachInfo) {
+	ensureMusicPlayingIffRequired();
+});
+
+chrome.windows.onFocusChanged.addListener(function(tabId, attachInfo) {
+	ensureMusicPlayingIffRequired();
+});
+
+chrome.tabs.onActivated.addListener(function(activeInfo) {
+	//activeInfo
+	// int tabId The ID of the tab that has become active
+	// int windowId The ID of the window the active tab changed inside of.
+
+	ensureMusicPlayingIffRequired();
+});
+
+chrome.webNavigation.onCompleted.addListener(function(details) {
+	ensureMusicPlayingIffRequired();
+});
+
+start();
+
+function tearDown() {
+	alive = false;
+	for(var i=0; i<state.rules.length; i++) {
+		var rule = state.rules[i];
+		ensureNotPlaying(rule.audio);
 	}
+}
 
-	(function initRules(rules) {
-	for (var i=0; i<rules.length; i++) {
-		var rule = rules[i];
-		rule.audio = new Audio();
-		var audio = rule.audio;
-		cue(audio, (rule.favouriteFirst && rule.playlist[0]) || pickNextSong(rule));
-
-		audio.addEventListener('ended', function() {	
-			cue(pickNextSong());
-		});
-	}
-	})(state.rules);
-
-	chrome.tabs.onRemoved.addListener(function(tabId, attachInfo) {
-		ensureMusicPlayingIffRequired();
-	});
-
-	chrome.windows.onFocusChanged.addListener(function(tabId, attachInfo) {
-		ensureMusicPlayingIffRequired();
-	});
-
-	chrome.tabs.onActivated.addListener(function(activeInfo) {
-		//activeInfo
-		// int tabId The ID of the tab that has become active
-		// int windowId The ID of the window the active tab changed inside of.
-
-		ensureMusicPlayingIffRequired();
-	});
-})
+chrome.storage.onChanged.addListener(function () {
+	tearDown();
+	start();
+});
